9d0bd785f6c1010c5c55ade70fae6de149a630f7
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2024-09-13 23:07:23 +0500

    Update addUserToSupabase.tsx

--- lib/addUserToSupabase.tsx
+++ lib/addUserToSupabase.tsx
@@ -4,47 +4,46 @@ import { useEffect, useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { createClient } from "@/supabase/client";
 
-export default function AddUserToSupabase() {
-    const { user } = useUser();
+export default async function AddUserToSupabase() {
+    const { user, isSignedIn } = useUser();
     const supabase = createClient()
-    const [signup, setSignUp] = useState(false)
 
-    useEffect(() => {
-        if (user) {
-            const userCreatedAt = user.createdAt ? new Date(user.createdAt) : null;
-            if (userCreatedAt) {
-                const now = new Date();
-                const timeDifference = Math.abs(now.getTime() - userCreatedAt.getTime());
-                const signupThreshold = 7 * 1000;
-                const isFirstSignUp = timeDifference < signupThreshold;
-                setSignUp(isFirstSignUp);
-                console.log("isFirstSignUp: ", isFirstSignUp);
-            }
-        }
-    }, [user]);
+    const newUser = {
+        id: user?.id,
+        email: user?.emailAddresses[0].emailAddress,
+        fullname: user?.fullName
+    }
 
-    const addUser = async () => {
-        const createdAt = user?.createdAt ? new Date(user.createdAt) : new Date();
-        const newUser = {
-            id: user?.id,
-            email: user?.emailAddresses[0].emailAddress,
-            fullname: user?.fullName,
-            created_at: createdAt
-        }
-        try {
-            const { error } = await supabase
-                .from('Users')
-                .insert(newUser)
-            if (error) {
-                console.log(error);
+    const { data, error } = await supabase
+        .from('Users')
+        .select('id')
+        .eq("id", user?.id)
+
+    if (error) {
+        console.error('Error fetching user exist data:', error);
+    } else {
+        if (data.length) {
+            console.log('Data:', data);
+        } else {
+            if (isSignedIn && data.length) {
+                try {
+                    const { error } = await supabase
+                        .from('Users')
+                        .insert(newUser)
+                    if (error) {
+                        console.log("error adding user: ", error);
+                    }
+                } catch (error) {
+                    console.log("error adding user: ", error);
+                }
             }
-        } catch (error) {
-            console.log("error: ", error);
         }
     }
 
-    if (signup) {
-        addUser()
-    }
+
+
+
+
+
 
 }
