3bd1aa89365e8c4a9c6c1732b94c3e2f92a7008e
r3hmantom <r3hmantom@gmail.com>
2024-09-14 11:39:00 +0500

    fix course page

--- app/(platform)/dashboard/courses/page.tsx
+++ app/(platform)/dashboard/courses/page.tsx
@@ -23,26 +23,7 @@ const CoursePage = () => {
   const [courses, setCourses] = useState<any[]>([]);
 
     return (
-    <>
-     <div>
-        <CourseList courses={courses} />
-     </div>
-     </>
-    )
-}
-
-export default CoursePage
-"use client"
-import React, { useEffect, useState } from "react"
-import { CourseList } from "./_components/courseList"
-
-
-const CoursePage = () => {
-
-  const [courses, setCourses] = useState([]);
-
-  return (
-    <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Explore Courses</h1>
       <div className="space-y-4">
         {courses.length > 0 ? (
@@ -51,8 +32,7 @@ const CoursePage = () => {
         }
       </div>
     </div>
-
-  )
+    )
 }
 
 export default CoursePage
