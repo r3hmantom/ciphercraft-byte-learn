2a519d47b8819d5197e6009020d00e24b0eba174
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2024-09-14 11:16:45 +0500

    design update in course

--- app/(platform)/dashboard/courses/_components/courseList.tsx
+++ app/(platform)/dashboard/courses/_components/courseList.tsx
@@ -1,32 +1,27 @@
 import React from 'react';
-import {CourseCard} from './courseCard';  // Assuming CourseCard is in the same directory
+import { CourseCard } from './courseCard';  // Assuming CourseCard is in the same directory
 
 
 interface CourseProps {
-    id: string;
-    name: string;
-    description: string;
-    numLesson: number;
-    imageUrl: string;
-    progress: number;
+  id: string;
+  name: string;
+  description: string;
+  numLesson: number;
+  imageUrl: string;
+  progress: number;
 };
 
 
 const CourseList = ({ courses }: { courses: CourseProps[] }) => {
   return (
-      <>
-      
-      {courses?.length === 0 ? (
-          <p className="text-center text-gray-500">No courses available at the moment.</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {courses.map((course) => (
-              <CourseCard key={course.id} course={course} />
-            ))}
-          </div>
-        )}
-       </>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {courses.map((course) => (
+          <CourseCard key={course.id} course={course} />
+        ))}
+      </div>
+    </>
   );
 };
 
-export {CourseList};
+export { CourseList };
--- app/(platform)/dashboard/courses/page.tsx
+++ app/(platform)/dashboard/courses/page.tsx
@@ -1,20 +1,24 @@
 "use client"
-import React, { useEffect , useState  } from "react"
-import {CourseList} from "./_components/courseList"
+import React, { useEffect, useState } from "react"
+import { CourseList } from "./_components/courseList"
 
 
 const CoursePage = () => {
 
   const [courses, setCourses] = useState([]);
 
-    return (
-    <>
-     <h2> Explore Courses </h2>
-     <div>
-        <CourseList courses={courses} />
-     </div>
-     </>
-    )
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Explore Courses</h1>
+      <div className="space-y-4">
+        {courses.length > 0 ? (
+          <CourseList courses={courses} />
+        ) : (<div className='text-lg'>No courses available at the moment.</div>)
+        }
+      </div>
+    </div>
+
+  )
 }
 
 export default CoursePage
